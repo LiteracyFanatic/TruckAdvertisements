cd9c9423a9522751bc93cb6583f333916a7fad3b
cannonjm <cannonjm@MSOE-CND52919BC>
2016-06-07 09:46:25 -0500

    Cleaned project and edited README.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Advertisement Website for Racine Truck Sales
 
+This website is a fully configurable advertisement system developed for [Racine Truck Salse ](http://www.volvotrucks.com/dealers-vtna/en-us/milwaukeetruck/Pages/index.aspx) where it will be used at the service desk. It is powered by [Jekyll](https://jekyllrb.com/) and is intended to run fullscreen in a television browser.
+
+If you'd like to use it for your own project, all you need to do is add your own assets to the img folder and edit the settings.yml file to update the text, images, videos, and colors.
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,36 +4,26 @@ $(document).ready(function () {
 });
 
 function attachResizeVideo() {
-
     $(window).resize(function () {
         resizeVideo();
     });
-
     resizeVideo();
 }
 
 function resizeVideo() {
-
-    console.log("fired");
-
     var $ytplayer = $('#ytplayer');
 
     var $parent = $ytplayer.parent();
 
-
-
     var newHeight = $parent.height();
 
     var aspectRatio = $ytplayer.get(0).height / $ytplayer.get(0).width;
-
-    console.log(aspectRatio);
 
     $ytplayer
        .height(newHeight)
        .width(newHeight / aspectRatio);
 
     if ($ytplayer.width() > $parent.width()) {
-        console.log('correction');
 
         var widthPadding = $ytplayer.outerWidth() - $ytplayer.width();
 
@@ -48,11 +38,11 @@ function resizeVideo() {
 }
 
 function switchAd() {
-
     $('.advertisement-wrapper').not(':first').hide();
 
-    setInterval(function () {
+    var transitionTime = $('#advertisements-container').attr('data-adTime') * 1000;
 
+    setInterval(function () {
         console.log('update');
         var $ads = $('.advertisement-wrapper');
         var len = $ads.length;
@@ -68,6 +58,5 @@ function switchAd() {
                 $ads.first().fadeIn(1000).addClass('active');
             }
         });
-
-}, 10000);
+    }, transitionTime);
 }
